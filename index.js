804e8dde5dfb75bac587cbcb8de63f3e9f4035a5
alexcastillla <alexander.zcas@gmail.com>
2020-10-12 11:50:09 +0000

    Adding exercise to GitHub

---
+++ index.js
@@ -0,0 +1,14 @@
+    let whoOptions = ['the dog','my grandma','my turtle','my bird','my fish'];
+    let didOptions = ['ate','peed','burned','broke'];
+    let objectOptions = ['my homework', 'my bag', 'the notebook'];
+    let timeOptions = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+function excuseGenerator(){
+
+    whoAnswer = whoOptions[Math.round(Math.random()*(whoOptions.length-1))];
+    didAnswer = didOptions[Math.round(Math.random()*(didOptions.length-1))];
+    objectAnswer = objectOptions[Math.round(Math.random()*(objectOptions.length-1))];
+    timeAnswer = timeOptions[Math.round(Math.random()*(timeOptions.length-1))];
+
+    document.getElementById("answerDocument").innerHTML = "Sorry teacher, but" +" " + whoAnswer +" "+ didAnswer +" "+ "objectAnswer" +" "+ timeAnswer;
+}
